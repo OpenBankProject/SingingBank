b0de9f380abe1e013682d86fb94ace24792b958f
Simon Redfern <simon AT tesobe DOT com>
2012-11-17 00:09:43 +0100

    string around host
    Signed-off-by: Simon Redfern <simon AT tesobe DOT com>

--- app.js
+++ app.js
@@ -357,7 +357,7 @@ app.get('/about', function(req, res) {
 
 
 var port = process.env.PORT || 3000;
-var host = process.env.HOST || 0.0.0.0;
+var host = process.env.HOST || "0.0.0.0";
 app.listen(port, host, function() {
   //console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
   console.log("Express server listening in %s mode", app.settings.env);
