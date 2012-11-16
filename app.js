ed9b3fa7d55048ea855ab27a1ec380ad82b88c4c
Simon Redfern <simon AT tesobe DOT com>
2012-11-17 00:03:47 +0100

    port
    Signed-off-by: Simon Redfern <simon AT tesobe DOT com>

--- app.js
+++ app.js
@@ -355,7 +355,10 @@ app.get('/about', function(req, res) {
     });
 });
 
-app.listen(settings.server.port, '127.0.0.1', function() {
+
+var port = process.env.PORT || 3000;
+
+app.listen(port, '127.0.0.1', function() {
   //console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
   console.log("Express server listening in %s mode", app.settings.env);
 
