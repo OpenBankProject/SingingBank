1e730b2197ea406776899dc07a1295a11dc108af
Stefan Bethge <stefan@tesobe.com>
2013-04-12 20:24:04 +0200

    paths

--- stop-dev.sh
+++ stop-dev.sh
@@ -5,7 +5,7 @@ LOG_FILE=$LOG_DIR/singingbank-dev.log
 
 PID_FILE=/var/run/nodejs-singingbank-dev.pid
 
-forever --pidFile $PID_FILE stop app.js
+/usr/local/bin/forever --pidFile $PID_FILE stop app.js
 echo "Stopping NodeJS app"
 echo "Stopping NodeJS app" >> $LOG_FILE
 exit 0
--- stop-live.sh
+++ stop-live.sh
@@ -5,7 +5,7 @@ LOG_FILE=$LOG_DIR/singingbank-live.log
 
 PID_FILE=/var/run/nodejs-singingbank-live.pid
 
-forever --pidFile $PID_FILE stop app.js
+/usr/local/bin/forever --pidFile $PID_FILE stop app.js
 echo "Stopping NodeJS app"
 echo "Stopping NodeJS app" >> $LOG_FILE
 exit 0
